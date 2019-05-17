5816899d01267deb79c6faf7d4bb6ca8b65fbbb2
canis617 <whwhdgus96@gmail.com>
2019-05-17 18:06:29 +0900

    classInfoDB : GetEmptyClass(building, day, time),GetSchedule(studentID), SetSchedule(StudentID, classID, classNum) , ClassInfo : GetKey(key)/return list of key

--- src/EmptyClass/ClassInfo.java
+++ src/EmptyClass/ClassInfo.java
@@ -1,7 +1,15 @@
 package EmptyClass;
 
-public class ClassInfo {
+import java.security.Key;
+import java.util.List;
+import java.util.Map;
 
+public class ClassInfo {
+	private int studentID;
+	List<Map> classList;
+	ClassInfoDB db;
+	/**
+	classList information
 	private String className;
 	private int classID;
 	private int splitClassNumber;
@@ -10,14 +18,28 @@ public class ClassInfo {
 	private String day;
 	private int startTime;
 	private int classLasting;
-	
-	public ClassInfo() {
-		//initializer of variables
+	 */
+
+	public ClassInfo(int studentID) {
+		this.studentID = studentID;
+		db = new ClassInfoDB();
+		classList = db.GetSchedule(this.studentID);
 	}
 
-	public String GetClassName() {
-		return className;
+	public void PrintSchedule(){
+		db.PrintListMap(classList);
 	}
-	
-	//get---set--- funciton
+
+	public List<String> GetKey(String key){
+		List<String> result = null;
+		Map<String, String> current;
+		for(int i=0; i<classList.size();i++){
+			current = classList.get(i);
+			result.add(current.get(key));
+			System.out.println();
+		}
+		return result;
+	}
+	//unit test
+
 }
--- src/EmptyClass/ClassInfoDB.java
+++ src/EmptyClass/ClassInfoDB.java
@@ -30,6 +30,10 @@ public class ClassInfoDB {
 
     //to set sql, get resultset from sql
     public List<Map> GetResultMap(String sql){
+        if(sql == null){
+            throw new IllegalArgumentException("parameter error");
+        }
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -91,6 +95,145 @@ public class ClassInfoDB {
         return null;
     }
 
+    public boolean SetData(String sql){
+        if(sql == null){
+            throw new IllegalArgumentException("parameter error");
+        }
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println(" !! <JDBC 오류> Driver load 오류: " + e.getMessage());
+            e.printStackTrace();
+        }
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://" + server + "/" + database + "?useSSL=false", user_name,password);
+            //connection debug
+            //System.out.println("정상적으로 연결되었습니다.");
+
+            state = conn.createStatement();
+            if(state.execute(sql) == true){
+                return true;
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if(state != null) {
+                    state.close();
+                }
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+            try {
+                if(conn != null){
+                    conn.close();
+                }
+            }catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+
+
+        return false;
+    }
+    //get empty class
+    public List<Map> GetEmptyClass(int building, String day, double time){
+        if(building < 0 || time<0 || time > 24 || (day != "mon" && day != "tues" && day !="wed" && day !="thurs" && day !="fri" &&day !="sat" && day !="sun")){
+            throw new IllegalArgumentException("parameter error");
+        }
+        String _time = Double.toString(time);
+        String _building = Integer.toString(building);
+        String sql;
+        sql = String.format("select distinct room from ClassInfo where day='%s' and building=%s and starttime<=%s and (starttime+lastingtime)>%s order by room",day, _building,_time, _time);
+        List<Map> beingClass = GetResultMap(sql);
+
+        sql = String.format("select distinct room from ClassInfo where building=%s order by room", _building);
+        List<Map> allClass = GetResultMap(sql);
+
+        List<Map> resultClass = exceptList(allClass, beingClass);
+        return resultClass;
+    }
+
+    //get schedule
+    //critical error !!
+    //Same classID, classNum classes are exists! -> This is for Class Information table
+    public List<Map> GetSchedule(int studentID){
+        if(studentID < 0 || studentID > 100000000){
+            throw new IllegalArgumentException("student ID error");
+        }
+        String _ID = Integer.toString(studentID);
+        String sql;
+        sql = String.format("select C.* from PersonalSchedule as P JOIN ClassInfo as C On P.classID=C.classID and P.classNum=C.classNum where P.studentID=%s",_ID);
+        List<Map> classList = GetResultMap(sql);
+        return classList;
+    }
+
+    public boolean SetSchedule(int studentID, int classID, int classNum){
+        if(studentID < 0 || studentID > 100000000 || classID <0 || classID > 100000 || classNum <0){
+            throw new IllegalArgumentException("schedule data error");
+        }
+        String _ID = Integer.toString(studentID);
+        String _CID = Integer.toString(classID);
+        String _CNum = Integer.toString(classNum);
+        String sql;
+        boolean success = false;
+        sql = String.format("insert into PersonalSchedule values('%s', %s, %s)",_ID, _CID,_CNum);
+        try {
+            success = SetData(sql);
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }finally {
+            System.out.println(sql + "successed");
+        }
+
+
+        return success;
+    }
+
+    //set schedule
+
+    //return a-b
+    public List<Map> exceptList(List<Map> a, List<Map>b){
+        int bCount=0;
+        Map<String, String> compareList = b.get(bCount);
+        for(int i=0; i<a.size();i++){
+            Map<String, String> current = a.get(i);
+            if(current.equals(compareList) == true){
+                a.remove(i--);
+                if(++bCount < b.size()){
+                    compareList = b.get(bCount);
+                }
+            }
+        }
+        return a;
+    }
+
+    public void PrintListMap(List<Map> tempList){
+
+
+        for(String key : current.keySet()){
+            String value = current.get(key);
+            if(key == "className"){
+                System.out.print(key +"\t\t\t\t");
+            }
+            else{
+                System.out.print(key +"\t\t");
+            }
+        }
+        System.out.println();
+
+        for(int i=0; i<tempList.size();i++){
+            current = tempList.get(i);
+            for(String key : current.keySet()){
+                String value = current.get(key);
+                System.out.print(value+"\t\t");
+            }
+            System.out.println();
+        }
+        System.out.println("size : "+tempList.size());
+    }
+
 
     public String GetServer() {
         return server;
@@ -104,6 +247,8 @@ public class ClassInfoDB {
     public String GetPassword(){
         return password;
     }
+
+
 
     //unit test for DB connection
     public static void main(String[] args) {
@@ -139,17 +284,22 @@ public class ClassInfoDB {
 
         try{
             ClassInfoDB test = new ClassInfoDB();
-            String sql = "Select distinct room From ClassInfo where building='310' ORDER BY room";
-            List<Map> tempList = test.GetResultMap(sql);
+            //
+            // String sql = "Select * From ClassInfo where building='310' ORDER BY room";
+            String sql;
 
-            for(int i=0; i<tempList.size();i++){
-                Map<String, String> current = tempList.get(i);
-                for(String key : current.keySet()){
-                    String value = current.get(key);
-                    System.out.println(key+" : "+value);
-                }
+            //get empty class test
+            List<Map> tempList = test.GetEmptyClass(310, "mon", 12);
+
+            //get student's class list
+            List<Map> studentClassList = test.GetSchedule(20150864);
+            test.PrintListMap(studentClassList);
+
+            try {
+                test.SetSchedule(20150864, 10000, 01);
+            }catch(Exception e){
+                System.out.println(e.getMessage());
             }
-
         }catch(Exception e){
             e.getMessage();
         }finally{
